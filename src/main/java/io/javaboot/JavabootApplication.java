6c2b70e7109fc44d7b14d893773aac9cef17d450
jinjun.yu <jinjun.yu@qq.com>
2023-03-07 17:41:26 +0800

    jdk17+springboot3=第一次提交

---
+++ src/main/java/io/javaboot/JavabootApplication.java
@@ -0,0 +1,17 @@
+package io.javaboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabootApplication {
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(JavabootApplication.class, args);
+        } catch (Exception e) {
+
+        }
+    }
+
+}
---
+++ src/test/java/io/javaboot/JavabootApplicationTests.java
@@ -0,0 +1,14 @@
+package io.javaboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabootApplicationTests {
+
+    @Test
+    void contextLoads() {
+        String a = "springframework";
+    }
+
+}
